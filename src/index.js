d0b813c86ec47fce0e9651daf045899089558088
Konrad Szalwinski <konrad.szalwinski@assertis.co.uk>
2016-11-14 12:14:25 +0100

    KSBWI-796 Added IconHeader component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 export { default as OutcomeButton } from './OutcomeButton/OutcomeButton';
-
 export { default as OutcomeButtonUI } from './OutcomeButton/OutcomeButtonUI';
-
 export { default as Header } from './Header/Header';
