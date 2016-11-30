4391b8cdbfb3766ee4b9612247e1912890b88825
Karol Maciaszek <karol@globalmouth.com>
2016-11-30 13:18:21 +0100

    KSBWI-843 variable width tabs in TabBarScrolled

--- src/TabPagination/TabBarScrolled.jsx
+++ src/TabPagination/TabBarScrolled.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Children, Component, PropTypes } from 'react';
 import styles from './TabBarScrolled.scss';
 import ArrowButton from './ArrowButton';
 import TabContainer from './TabContainer';
@@ -38,6 +38,7 @@ class TabBarScrolled extends Component {
       };
 
       this.scrollLeft = 0;
+      this.tabWidths = [];
 
       this.prevPage = this.prevPage.bind(this);
       this.nextPage = this.nextPage.bind(this);
@@ -117,23 +118,15 @@ class TabBarScrolled extends Component {
    }
 
    /**
-    * Single tab width (in pixels)
+    * Returns maximal scroll left offset.
     * @returns {number|null}
     */
-   get tabWidth() {
-      return this.firstTabWidth;
-   }
-
-   /**
-    * Returns maximal scroll left offset.
-    * @returns {number}
-    */
    get maxScrollLeft() {
-      if (!(this.tabWidth && this.eyeshotWidth)) {
+      if (!(this.eyeshotWidth)) {
          return null;
       }
 
-      return this.props.children.length * this.tabWidth - this.eyeshotWidth;
+      return this.computeTabsWidth(0, Children.count(this.props.children) - 1) - this.eyeshotWidth;
    }
 
    /**
@@ -141,7 +134,7 @@ class TabBarScrolled extends Component {
     * @param {number} offset Scroll offset
     */
    scrollTo(offset) {
-      if (!(this.bar && this.tabWidth && this.eyeshotWidth && this.eyeshot)) {
+      if (!(this.bar && this.maxScrollLeft && this.eyeshotWidth && this.eyeshot)) {
          return;
       }
 
@@ -175,11 +168,11 @@ class TabBarScrolled extends Component {
     * @param {number} tab Tab index
     */
    scrollToTab(tab) {
-      if (!(this.bar && this.tabWidth && this.eyeshotWidth)) {
+      if (!(this.bar && this.eyeshotWidth)) {
          return;
       }
 
-      this.scrollTo(tab * this.tabWidth - (this.eyeshotWidth - this.tabWidth) / 2);
+      this.scrollTo(this.computeTabsWidth(0, tab) - (this.eyeshotWidth - this.computeTabsWidth(tab)) / 2);
    }
 
    /**
@@ -199,17 +192,33 @@ class TabBarScrolled extends Component {
    }
 
    /**
+    * Returns tab widths sum for given range.
+    * @param {number} start First tab index
+    * @param {number} end Last tab index
+    * @returns {number}
+    */
+   computeTabsWidth(start, end = start) {
+      return this.tabWidths
+         .slice(start, end)
+         .reduce((sum, tabWidth) => sum + (tabWidth ? tabWidth : 0), 0);
+   }
+
+   get averageTabWidth() {
+      return this.computeTabsWidth(0, Children.count(this.props.children) - 1) / Children.count(this.props.children);
+   }
+
+   /**
     * Scrolls tab bar to previous page.
     */
    prevPage() {
-      this.scrollTo(this.scrollLeft - this.props.step * this.tabWidth);
+      this.scrollTo(this.scrollLeft - this.props.step * this.averageTabWidth);
    }
 
    /**
     * Scrolls tab bar to next page.
     */
    nextPage() {
-      this.scrollTo(this.scrollLeft + this.props.step * this.tabWidth);
+      this.scrollTo(this.scrollLeft + this.props.step * this.averageTabWidth);
    }
 
    /**
@@ -221,11 +230,11 @@ class TabBarScrolled extends Component {
          <div className={styles.container} ref={el => (this.container = el)}>
             <div className={styles.eyeshot} ref={el => (this.eyeshot = el)}>
                <div className={styles.bar} ref={el => (this.bar = el)}>
-                  {this.props.children.map((child, i) => this.props.renderTabContainer({
+                  {Children.map(this.props.children, (child, i) => this.props.renderTabContainer({
                      key: i,
                      selected: this.state.tab == i,
                      onClick: this.onTabClick.bind(this, i),
-                     onWidth: i == 0 ? width => (this.firstTabWidth = width) : undefined,
+                     onWidth: width => (this.tabWidths[i] = width),
                      children: child
                   }))}
                </div>
