8249513ab2373fd859f225efe9982f799ecbdcf4
Karol Maciaszek <karol@globalmouth.com>
2016-12-07 17:01:29 +0100

    KSBWI-853 OutcomeButtonUI can be rendered without odds, label only

--- src/OutcomeButton/OutcomeButtonUI.jsx
+++ src/OutcomeButton/OutcomeButtonUI.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from './OutcomeButtonUI.scss';
 
 /**
  * Renders the UI of an outcome button, no
@@ -6,80 +7,75 @@ import React, { Component } from 'react';
  * @memberof widget-components
  */
 class OutcomeButtonUI extends Component {
-   get outcomeOdds() {
-      return (
-         <div className='KambiWidget-outcome__odds-wrapper'>
-            <span className='KambiWidget-outcome__odds'>
-               {this.props.odds}
-            </span>
-         </div>
-      )
-   }
 
    render() {
       const { label, odds, suspended, selected, onClick } = this.props;
+
       let buttonCssClasses = 'l-flex-1 kw-link KambiWidget-outcome';
+
       if (suspended) {
          buttonCssClasses += ' KambiWidget-outcome--suspended';
       } else if (selected) {
          buttonCssClasses += ' KambiWidget-outcome--selected';
       }
-      if (label == null) {
-         return (
-            <button
-               type='button'
-               role='button'
-               className={buttonCssClasses}
-               disabled={suspended}
-               onClick={onClick}
-            >
-               <div className='l-flexbox l-pack-center'>
-                  { this.outcomeOdds }
-               </div>
-            </button>
-         );
-      } else {
-         return (
-            <button
-               type='button'
-               role='button'
-               className={buttonCssClasses}
-               disabled={suspended}
-               onClick={onClick}
-            >
+
+      if (odds === null && label === null) {
+         throw new Error('Both odds and label cannot be set to null');
+      }
+
+      return (
+         <button
+            type='button'
+            role='button'
+            className={buttonCssClasses}
+            disabled={suspended}
+            onClick={onClick}
+         >
+            {odds !== null && label !== null &&
                <div className='KambiWidget-outcome__flexwrap'>
                   <div className='KambiWidget-outcome__label-wrapper'>
-                     <span className='KambiWidget-outcome__label'>
-                        {label}
-                     </span>
+                     <span className='KambiWidget-outcome__label'>{label}</span>
                      <span className='KambiWidget-outcome__line' />
                   </div>
-                  { this.outcomeOdds }
-               </div>
-            </button>
-         );
-      }
+                  <div className='KambiWidget-outcome__odds-wrapper'>
+                     <span className='KambiWidget-outcome__odds'>{odds}</span>
+                  </div>
+               </div>}
+
+            {odds !== null && label === null &&
+               <div className='KambiWidget-outcome__odds-wrapper'>
+                  <span className='KambiWidget-outcome__odds'>{odds}</span>
+               </div>}
+
+            {odds === null && label !== null &&
+               <div className={`KambiWidget-outcome__label-wrapper ${styles.label}`}>
+                  <span className='KambiWidget-outcome__label'>{label}</span>
+               </div>}
+         </button>
+      );
    }
 }
 
 /**
  * @property [label=null] {string?} if not defined centralizes the odds in the button, if defined uses this as the label in the button
- * @property odds {string} The odds to show in the button
+ * @property [odds=null] {string?} if not defined centralizes the label in the button, if defined shows the odds in the button (either centralized or on the right side if label is defined)
  * @property [suspended=false] {boolean} If true the button is greyed out
  * @property selected {boolean} If true the button is selected, false otherwise.
  * @property onClick {Function} Callback for when the button is clicked
  */
 OutcomeButtonUI.propTypes = {
    label: React.PropTypes.string,
-   odds: React.PropTypes.string.isRequired,
+   odds: React.PropTypes.string,
    suspended: React.PropTypes.bool,
    selected: React.PropTypes.bool.isRequired,
    onClick: React.PropTypes.func
 };
 
 OutcomeButtonUI.defaultProps = {
+   odds: null,
    label: null,
-   suspended: false
+   suspended: false,
+   selected: false
 };
 
 export default OutcomeButtonUI;
