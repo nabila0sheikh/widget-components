b1d1efbe8a7669eaa40ddf053fc9b5c7f0a32465
angelicahl88 <angelica@globalmouth.com>
2018-03-02 10:45:40 +0100

    updated IconHeader to take localStyles

--- src/IconHeader/IconHeader.jsx
+++ src/IconHeader/IconHeader.jsx
@@ -2,22 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './IconHeader.scss';
 
-const IconHeader = ({ title, subtitle, children }) => (
-   <header className={`KambiWidget-card-text-color KambiWidget-card-border-color KambiWidget-font ${styles.header}`}>
-      <div className={styles.icon}>{children}</div>
-      <div className={styles.container}>
-         <div className={styles.title} title={title}>{title}</div>
-         <div className={styles.subtitle} title={subtitle}>{subtitle}</div>
-      </div>
-   </header>
-);
+const IconHeader = ({ title, subtitle, children, localStyles=[] }) => {
+   const iconStyles = [styles.icon]
+   if (localStyles.length > 0) {
+      localStyles.forEach(style => iconStyles.push(style))
+   }
+   return (
+      <header className={`KambiWidget-card-text-color KambiWidget-card-border-color KambiWidget-font ${styles.header}`}>
+         <div className={iconStyles.join(' ')}>
+            {children}
+         </div>
+         <div className={styles.container}>
+            <div className={styles.title} title={title}>{title}</div>
+            <div className={styles.subtitle} title={subtitle}>{subtitle}</div>
+         </div>
+      </header>
+   )
+}
 
 /**
+ * @property [localStyles] {Array} Array of local styles as strings
  * @property [children] {ReactElement} Header's icon markup
  * @property title {string} Header's title
  * @property [subtitle] {string} Header's subtitle
  */
 IconHeader.propTypes = {
+   localStyles: PropTypes.arrayOf(PropTypes.string),
    children: PropTypes.node,
    title: PropTypes.string.isRequired,
    subtitle: PropTypes.string
